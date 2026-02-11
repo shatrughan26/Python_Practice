418e0e9a9f42d0e903dde335f5b58360f64c5dfe
Deepak kumar sharma <deepak4996sharma@gmail.com>
2026-02-11 23:33:45 +0530

    Learning what is Modules, Packages and library

---
+++ Module-Packages/module.py
@@ -0,0 +1,20 @@
+# import mymodule
+# mymodule.say_hello("Alice")
+# mymodule.say_goodbye("Bob")
+
+# from mymodule import person1
+# print(person1['name'])
+# print(person1['age'])
+
+
+#library
+#in built library
+import math 
+A = 165
+print(math.sqrt(A))
+
+
+#import specific function from library
+from math import factorial
+B = 4
+print(factorial(B))
---
+++ Module-Packages/mymodule.py
@@ -0,0 +1,8 @@
+person1 = {'name': 'Alice', 'age': 30}
+
+def say_hello(name):
+    return print(f"Hello, {name}! kese ho?")
+
+def say_goodbye(name):
+    return print(f"Goodbye, {name}! milte hai phir se")
+
